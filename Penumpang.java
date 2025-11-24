fdd827d759a69529725f1179a62edc26b017b7f0
AndreaWidi <andreawidi886@gmail.com>
2025-11-24 15:45:00 +0700

    Update Penumpang.java

--- Penumpang.java
+++ Penumpang.java
@@ -8,3 +8,10 @@ public class Penumpang implements BayarBus {
     protected Kategori kategori;
 
     public Penumpang(int id, String nama, int umur, boolean hamil) {
+        this.id = id;
+        this.nama = nama;
+        this.umur = umur;
+        this.hamil = hamil;
+        this.saldo = 10000;
+        this.kategori = Kategori.BIASA;
+    }
