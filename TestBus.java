647765ca929ce2316b123297977442aa2363b3c7
AndreaWidi <andreawidi886@gmail.com>
2025-11-27 16:18:09 +0700

    Update TestBus.java

--- TestBus.java
+++ TestBus.java
@@ -1,1 +1,9 @@
+import java.util.Scanner;
 
+public class TestBus {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        Bus bus = new Bus();
+
+        while (true) {
